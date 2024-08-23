91bc5ee61f94a1f0c9c25cd74d0efb163515714e
Jordan Bursik <jsbursik@gmail.com>
2024-08-22 19:15:59 -0500

    added the type for the config

--- src/config/auth.config.ts
+++ src/config/auth.config.ts
@@ -1,12 +1,13 @@
-import MicrosoftEntraId from '@auth/core/providers/microsoft-entra-id';
+import MicrosoftEntraId from "@auth/core/providers/microsoft-entra-id";
+import { ExpressAuthConfig } from "@auth/express";
 
-export const authConfig = {
+export const authConfig: ExpressAuthConfig = {
   trustHost: true,
   providers: [
     MicrosoftEntraId({
       clientId: process.env.ENTRA_CLIENT_ID,
       clientSecret: process.env.ENTRA_CLIENT_SECRET,
-      tenantId: process.env.ENTRA_TENANT_ID
+      tenantId: process.env.ENTRA_TENANT_ID,
     }),
-  ]
-}
+  ],
+};
